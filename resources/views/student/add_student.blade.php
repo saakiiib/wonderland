069ad49f338ed5fa1347dd300d7eb40b44ccfd63
saakiiib <128025804+saakiiib@users.noreply.github.com>
2023-11-11 15:59:40 +0600

    Added validation error if empty for add_student

--- resources/views/student/add_student.blade.php
+++ resources/views/student/add_student.blade.php
@@ -19,18 +19,27 @@
                         <input type="text" id="basic-addon-name" name="name" class="form-control" placeholder="Student"
                             aria-label="Name" aria-describedby="basic-addon-name" required />
                         <div class="valid-feedback">Looks good!</div>
+                        @if ($errors->has('name'))
+                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                        @endif
                     </div>
                     <div class="mb-1">
                         <label class="form-label" for="basic-default-email1">Email</label>
                         <input type="email" id="basic-default-email1" name="email" class="form-control"
                             placeholder="student@example.com" aria-label="student@example.com" required />
                         <div class="valid-feedback">Looks good!</div>
+                        @if ($errors->has('email'))
+                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                        @endif
                     </div>
                     <div class="mb-1">
                         <label class="form-label" for="basic-default-email1">Mobile Number</label>
                         <input type="number" id="contact-info" name="mobile_number" class="form-control"
                             placeholder="01xxxxxxxxx" required />
                         <div class="valid-feedback">Looks good!</div>
+                        @if ($errors->has('mobile_number'))
+                        <span class="text-danger">{{ $errors->first('mobile_number') }}</span>
+                        @endif
                     </div>
                     <div class="mb-1">
                         <label class="form-label">Class</label>
@@ -42,6 +51,9 @@
                             <option value="4">4</option>
                             <option value="5">5</option>
                         </select>
+                        @if ($errors->has('class_id'))
+                        <span class="text-danger">{{ $errors->first('class_id') }}</span>
+                        @endif
                     </div>
                     <button type="submit" class="btn btn-primary">Save</button>
                 </form>
