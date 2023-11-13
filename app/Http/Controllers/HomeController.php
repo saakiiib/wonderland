8984ed8e35c7e76a4877c0ed16b2965b871b374a
Sakib <nsakib.miraz@gmail.com>
2023-11-14 00:32:31 +0600

    Updated the controller for image upload and created migration file for image.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,15 +19,25 @@ class HomeController extends Controller
         $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
-        DB::table('home_data')->updateOrInsert(
-            ['id' => 1],
-            [
-                'title' => $request->input('title'),
-                'description' => $request->input('description')
-            ]
-        );
+        $data = [
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+        ];
+
+        //home
+        // $filename = time() . '.' . $request->image->extension();
+        // $request->image->move(public_path('home'), $filename);
+        //home
+
+        //storage 
+        $filename = time() . '.' . $request->image->extension();
+        $request->image->storeAs('public/home', $filename);
+        //storage
+
+        DB::table('home_data')->updateOrInsert(['id' => 1], $data);
 
         return redirect()->route('edit-home')->with('success', 'Home data updated successfully');
     }
